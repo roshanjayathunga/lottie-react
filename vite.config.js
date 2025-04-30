0eaa838b3d7fcce07e50a30db63e0b90e54e95b1
roshanjayathunga <roshanmjayathunga@gmail.com>
2025-04-30 14:51:01 +0200

    vite configs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/lottie-react/", // Replace with your repository name
+  base: "https://roshanjayathunga.github.io/", // Replace with your repository name
   plugins: [react()],
 });
